63b7ac1c01f76995cc66bf86d927444a971a5fb9
Joseph Azumah <azumahjoseph72@gmail.com>
2026-07-27 23:04:30 +0000

    Fix import statement for Firebase SDK
    Signed-off-by: Joseph Azumah <azumahjoseph72@gmail.com>

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 
 // Firebase SDK
-Firebase { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
   createUserWithEmailAndPassword,
